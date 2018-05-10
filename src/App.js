51be3fe144d6da98989eaf311d73bee545926c07
Morgan Fudge <mfudge92@gmail.com>
2018-05-10 11:51:53 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var profile = (
+  React.createElement("div", null,
+  React.createElement("img", { src: "avatar.png", className: "profile" }),
+  React.createElement("h3", null, [user.firstName, user.lastName].join(" ")))
+);
+
 class App extends Component {
   render() {
     return (
