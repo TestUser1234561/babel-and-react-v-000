e32b14bd230428066e83545c5086a9881476f199
Morgan Fudge <mfudge92@gmail.com>
2018-05-10 11:52:00 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 var profile = (
-  React.createElement("div", null,
-  React.createElement("img", { src: "avatar.png", className: "profile" }),
-  React.createElement("h3", null, [user.firstName, user.lastName].join(" ")))
-);
+  <div>
+    <img src="avatar.png" className="profile" />
+    <h3>{[user.firstName, user.lastName].join(' ')}</h3>
+  </div>;
+)
 
 class App extends Component {
   render() {
